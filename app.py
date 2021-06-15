b4fa57c29b41c2421a5583864c369d4b5b38ca89
kolinkorr839 <bichonfrise74@gmail.com>
2021-06-15 09:13:14 -0700

    add elasticsearch search and sample jinja inheritance

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 #!/bin/python3
 
+from elasticsearch import Elasticsearch
 from flask import Flask, request, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -40,6 +42,71 @@ def bmi_calculator():
         result = float(weight / height**2)
 
     return render_template("bmi.html", result=result)
+
+@app.route('/elasticsearch_simple')
+def elasticsearch_1():
+    es = Elasticsearch(['http://localhost:9200'])
+    search_query = {
+      "query": {
+        "term": {
+          "variable.raw": "AUTH_API_MYSQL_PORT"
+        }
+      }
+    }
+
+    result = es.search(index='invision-env', body=search_query)
+    for hit in result['hits']['hits']:
+        print("%(service)s - %(context)s: %(variable)s" % hit['_source'])
+    hits = result['hits']['total']['value']
+    return render_template("elasticsearch_simple.html", hits=hits, result=result)
+
+
+@app.route('/blank')
+def blank():
+    return render_template('blank.html')
+
+
+@app.route('/elasticsearch_static')
+def elasticsearch_2():
+    es = Elasticsearch(['http://localhost:9200'])
+    search_query = {
+      "query": {
+        "term": {
+          "variable.raw": "AUTH_API_MYSQL_PORT"
+        }
+      }
+    }
+
+    result = es.search(index='invision-env', body=search_query)
+    for hit in result['hits']['hits']:
+        print("%(service)s - %(context)s: %(variable)s" % hit['_source'])
+    hits = result['hits']['total']['value']
+    return render_template("elasticsearch_static.html", hits=hits, result=result)
+
+
+@app.route('/elasticsearch_dynamic', methods=['GET', 'POST'])
+def elasticsearch_3():
+    es = Elasticsearch(['http://localhost:9200'])
+
+    variable = ''
+    if request.method == 'POST':
+        variable = request.form['variable']
+    else:
+        return render_template("elasticsearch_dynamic.html", hits=0, result=0)
+
+    search_query = {
+      "query": {
+        "term": {
+          "variable.raw": "%s" %(variable)
+        }
+      }
+    }
+
+    result = es.search(index='invision-env', body=search_query)
+    for hit in result['hits']['hits']:
+        print("%(service)s - %(context)s: %(variable)s" % hit['_source'])
+    hits = result['hits']['total']['value']
+    return render_template("elasticsearch_dynamic.html", hits=hits, result=result)
 
 
 if __name__ == '__main__':
