4e92db9887736b15dec8a42f6baaa47f935ee16b
kolinkorr839 <bichonfrise74@gmail.com>
2021-06-18 09:05:05 -0700

    play with sqlalchmey

--- basic_app.py
+++ basic_app.py
@@ -1,10 +1,61 @@
 #!/bin/python3
 
 from elasticsearch import Elasticsearch
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+
+import os.path
+
 
 app = Flask(__name__)
+db_file = 'students.sqlite3'
+app.config ['SQLALCHEMY_DATABASE_URI'] = "sqlite:///%s" % (db_file)
+app.config['SECRET_KEY'] = 'ZtFeSuKxInjvEH72'
 
+db = SQLAlchemy(app)
+
+class students(db.Model):
+    id = db.Column('student_id', db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    city = db.Column(db.String(50))
+    addr = db.Column(db.String(200))
+    pin = db.Column(db.String(10))
+
+    def __init__(self, name, city, addr, pin):
+        self.name = name
+        self.city = city
+        self.addr = addr
+        self.pin = pin
+
+@app.route('/db_show_all')
+def db_show_all():
+    all_data = students.query.all()
+
+    # count = students.query.count()
+    count = db.session.query(students).count()
+
+    get_some_data = students.query.filter(students.pin == 94597).limit(2)
+    return render_template('db_show_all.html',
+        all_students=all_data, count=count,
+        get_some_data=get_some_data)
+
+
+@app.route('/db_new', methods=['GET', 'POST'])
+def db_new():
+    if request.method == 'POST':
+       if not request.form['name'] or not request.form['city'] or not request.form['addr']:
+          flash('Please enter all the fields', 'error')
+       else:
+          student = students(request.form['name'], request.form['city'],
+             request.form['addr'], request.form['pin'])
+
+          db.session.add(student)
+          db.session.commit()
+
+          flash('Record was successfully added')
+          return redirect(url_for('db_show_all'))
+
+    return render_template('db_new.html')
 
 @app.route('/')
 def welcome():
@@ -46,4 +97,6 @@ def bmi_calculator():
 
 
 if __name__ == '__main__':
+    if not os.path.isfile(db_file):
+        db.create_all()
     app.run(debug=True)
